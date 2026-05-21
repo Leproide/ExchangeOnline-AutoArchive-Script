2c90ffa839340fc34390de246ad20effb28d7835
Mauro <mauro.mossino91@gmail.com>
2026-05-21 14:57:15 +0200

    Update ITA_Enable-ExArchive.ps1

--- ITA_Enable-ExArchive.ps1
+++ ITA_Enable-ExArchive.ps1
@@ -6,7 +6,6 @@ https://github.com/Leproide/ExchangeOnline-AutoArchive-Script
 .LICENSE
 GPL v3 https://www.gnu.org/licenses/gpl-3.0.html
 
-
 .SYNOPSIS
     Abilita l'archivio online Exchange, imposta la retention policy e avvia l'archiviazione per un utente.
 
